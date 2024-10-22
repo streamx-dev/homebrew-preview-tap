e33a13d5e02b91ad81bcae7c40fd40c2f56bfb86
jreleaserbot <jreleaser@kordamp.org>
2024-10-22 09:37:59 +0000

    streamx 0.3.8-16-gaf01ca2

--- Formula/streamx.rb
+++ Formula/streamx.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.12.0 at 2024-10-21T12:49:17.696103458Z
+# Generated with JReleaser 1.12.0 at 2024-10-22T09:37:53.183458146Z
 
 class Streamx < Formula
   desc "CLI for StreamX - preview version"
   homepage "https://www.streamx.dev"
-  url "https://github.com/streamx-dev/streamx-cli-preview/releases/download/0.3.8-15-g45d03e5/streamx-0.3.8-15-g45d03e5.zip"
-  version "0.3.8-15-g45d03e5"
-  sha256 "8cc830d89e3ee8d5e9cd41dd6852a5d7fa2723582386706a431774c155a82cf9"
+  url "https://github.com/streamx-dev/streamx-cli-preview/releases/download/0.3.8-16-gaf01ca2/streamx-0.3.8-16-gaf01ca2.zip"
+  version "0.3.8-16-gaf01ca2"
+  sha256 "e790c7d8ff82ac5bbfe307eb46e9b5e7e70ad99fe9fe998c762d7382b1aea12a"
   license "EULA"
 
 
@@ -16,6 +16,6 @@ class Streamx < Formula
 
   test do
     output = shell_output("#{bin}/streamx --version")
-    assert_match "0.3.8-15-g45d03e5", output
+    assert_match "0.3.8-16-gaf01ca2", output
   end
 end
