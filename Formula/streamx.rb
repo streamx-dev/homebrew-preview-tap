4512813f85257657bd3f47b96233c99e8074e90a
jreleaserbot <jreleaser@kordamp.org>
2025-04-14 13:25:08 +0000

    streamx 1.0.2-24-g9c5c3d5

--- Formula/streamx.rb
+++ Formula/streamx.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.17.0 at 2025-04-14T13:24:14.423355705Z
+# Generated with JReleaser 1.17.0 at 2025-04-14T13:24:55.318630652Z
 
 class Streamx < Formula
   desc "CLI for StreamX - preview version"
   homepage "https://www.streamx.dev"
   url "https://github.com/streamx-dev/streamx-cli-preview/releases/download/1.0.2-24-g9c5c3d5/streamx-1.0.2-24-g9c5c3d5.zip"
   version "1.0.2-24-g9c5c3d5"
-  sha256 "7e8e0ffe54bf12e78665906740b2d47c023cea3f542309954810817f7a892ecb"
+  sha256 "1bdf245762ab76e010b6922cf1f2231676361bbb508f746732e2051f2e640020"
   license "EULA"
 
 
